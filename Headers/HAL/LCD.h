c036d203ab9104c0426775a4c30c1cdc7f2e5bc3
Kareem <43302105+Kiyk0@users.noreply.github.com>
2024-05-02 03:01:47 +0300

    LCD Header file

---
+++ Headers/HAL/LCD.h
@@ -0,0 +1,17 @@
+#define LCD GPIOB    		//LCD port with Tiva C 
+#define RS 0x01				 	//RS -> PB0 (0x01)
+#define RW 0x02         //RW -> PB1 (0x02)
+#define EN 0x04  		 	 	//EN -> PB2 (0x04)
+
+
+//Functions Declaration
+void delayUs(int);   														   //Delay in Micro Seconds
+void delayMs(int);   														   //Delay in Milli Seconds
+void LCD4bits_Init(void);													 //Initialization of LCD Dispaly
+void LCD_Write4bits(unsigned char, unsigned char); //Write data as (4 bits) on LCD
+void LCD_WriteString(char*);											 //Write a string on LCD 
+void LCD4bits_Cmd(unsigned char);									 //Write command 
+void LCD4bits_Data(unsigned char);								 //Write a character
+void displayLine1(char* str);
+void displayLine2(char* str);
+void displayWithCursor(char* str, char cursor);
