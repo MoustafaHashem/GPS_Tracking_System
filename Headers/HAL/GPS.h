037886029b1fc4553335cea134e58b46870a9aab
Mayar0311 <139752266+Mayar0311@users.noreply.github.com>
2024-05-02 12:43:42 +0300

    Add files via upload

---
+++ Headers/HAL/GPS.h
@@ -0,0 +1,11 @@
+
+void readData();
+void doFilter();
+float calculatDesenation(float long1,float lat1,float long2,float lat2);
+float getP1Long();
+float getP1Lat();
+float getspeed();
+float getP2Long();
+float getP2Lat();
+void setP2Long(float x);
+void setP2Lat(float x);
